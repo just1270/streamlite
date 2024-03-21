33f7dec5a8694038e4d3432c97cda18c91264f65
DESKTOP-MB6F5PE\admin <a007005002@gmail.com>
2024-03-21 14:34:24 +0800

    2

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import altair as alt
 
-st.write(1234)
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
-}))
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
